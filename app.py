6ae92149922a8c700a93429ab828697f264503a5
bhctsntrk <behcet.senturk@protonmail.com>
2019-12-02 00:29:42 +0300

    Main components added. Css and readme need to update.

--- app.py
+++ app.py
@@ -1,114 +1,385 @@
-import numpy as np
-from textwrap import dedent
 import datetime
 
-import plotly.graph_objs as go
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import dash_daq as daq
 
-from getOsInfo import getCpuTemp, getGpuTemp
+from getOsInfo import getCpuTemp, getGpuTemp, getCpuUsage, getMemUsage, getEverything
 
 # Define the app
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
 )
 server = app.server
-app.config.suppress_callback_exceptions = True
+# app.config.suppress_callback_exceptions = True
 
 # Font and background colors associated with each theme
-banner_color = {"dark": "#23262e", "light": "#ffffff"}
-bkg_color = {"dark": "#23262e", "light": "#f6f6f7"}
-grid_color = {"dark": "#53555B", "light": "#969696"}
-text_color = {"dark": "#95969A", "light": "#595959"}
-card_color = {"dark": "#2D3038", "light": "#FFFFFF"}
-accent_color = {"dark": "#FFD15F", "light": "#ff9827"}
+headerColor = {"dark": "#2D3038", "light": "#FFFFFF"}
+backgroundColor = {"dark": "#23262E", "light": "#F6F6F7"}
+gridColor = {"dark": "#53555B", "light": "#969696"}
+textColor = {"dark": "#95969A", "light": "#595959"}
+cardColor = {"dark": "#2D3038", "light": "#FFFFFF"}
+lightColor = {"dark": "#00ff55", "light": "#45fffd"}
+bckLightColor = {"dark": "#072611", "light": "#032b2a"}
 
 theme = "light"
 
-app.layout = html.Div(
-    html.Div([
-        html.H4('OS Monitoring Application'),
-        daq.LEDDisplay(
-            id="cpuTempLiveLed",
-            label="Cpu Temp",
-            value=0,
-            color=accent_color[theme],
-        ),
-        daq.LEDDisplay(
-            id="gpuTempLiveLed",
-            label="Gpu Temp",
-            value=0,
-            color=accent_color[theme],
-        ),
-        dcc.Graph(id='tempLiveGraph'),
-        dcc.Interval(
-            id='intervalCounter',
-            interval=1*1000, # in milliseconds
-            n_intervals=0
-        )
+tempDict = {"cpu": [], "gpu": [], 'time': []}
+
+usageDict = {"cpu": [], "mem": [], 'time': []}
+
+
+def bodyLayoutGen():
+    global theme
+
+    bodyLayout = html.Div(id="body", children=[
+        html.Div(id="tempSection", style={"backgroundColor": cardColor[theme]}, children=[
+            html.H5("System Temprature"),
+            dcc.Interval(
+                id='tempInterval',
+                interval=1*1000,  # in milliseconds
+                n_intervals=0
+            ),
+            html.Div(
+                id="tempGraphSection",
+                className="seven columns",
+                style={"backgroundColor": cardColor[theme]},
+                children=[
+                    html.Div(
+                        id="tempLedDispSection",
+                        style={
+                            "backgroundColor": backgroundColor[theme],
+                            "color": textColor[theme],
+                        },
+                        children=[
+                            daq.LEDDisplay(
+                                id="cpuTempLiveLed",
+                                label="Cpu Temp",
+                                value=0,
+                                color=lightColor[theme],
+                            ),
+                            daq.LEDDisplay(
+                                id="gpuTempLiveLed",
+                                label="Gpu Temp",
+                                value=0,
+                                color=lightColor[theme],
+                            ),
+                        ]
+                    ),
+                    dcc.Graph(id="tempLiveGraph", style={"width": "100%"}),
+                ]
+            ),
+            html.Div(
+                id="tempControlSection",
+                className="two columns",
+                style={"backgroundColor": backgroundColor[theme]},
+                children=[
+                    html.Button("Reset Graph",
+                                id="tempDataResetButton", n_clicks=0, style={"color": textColor[theme]}),
+                    daq.PowerButton(
+                        id="tempStopButton",
+                        color=lightColor[theme],
+                        on=True,
+                        size=120,
+                    ),
+                ]
+            )
+        ]),
+        html.Div(id="usageSection", style={"backgroundColor": cardColor[theme]}, children=[
+            html.H5("System Usage"),
+            dcc.Interval(
+                id='usageInterval',
+                interval=5*1000,  # in milliseconds
+                n_intervals=0
+            ),
+            html.Div(
+                id="usageGraphSection",
+                className="seven columns",
+                style={"backgroundColor": cardColor[theme]},
+                children=[
+                    html.Div(
+                        id="usageGaugeDispSection",
+                        style={
+                            "backgroundColor": backgroundColor[theme],
+                            "color": textColor[theme],
+                        },
+                        children=[
+                            daq.Gauge(
+                                id='cpuUsageLiveGauge',
+                                min=0,
+                                value=0,
+                                max=100,
+                                label="Cpu Usage",
+                                color=lightColor[theme],
+                            ),
+                            daq.Gauge(
+                                id='memUsageLiveGauge',
+                                min=0,
+                                value=0,
+                                label="Mem Usage",
+                                max=100,
+                                color=lightColor[theme],
+                            ),
+                        ]
+                    ),
+                    dcc.Graph(id="usageLiveGraph", style={"width": "100%"}),
+                ]
+            ),
+            html.Div(
+                id="usageControlSection",
+                className="two columns",
+                style={"backgroundColor": backgroundColor[theme]},
+                children=[
+                    html.Button("Reset Graph",
+                                id="usageDataResetButton", n_clicks=0, style={"color": textColor[theme]}),
+                    daq.PowerButton(
+                        id="usageStopButton",
+                        color=lightColor[theme],
+                        on=True,
+                        size=120,
+                    ),
+                ]
+            )
+        ])
     ])
+
+    if theme == "dark":
+        return daq.DarkThemeProvider(children=bodyLayout)
+    if theme == "light":
+        return bodyLayout
+
+
+app.layout = html.Div(
+    id="mainPage",
+    className="container",
+    style={"backgroundColor": backgroundColor[theme]},
+    children=[
+        html.Div(
+            id="header",
+            style={
+                "backgroundColor": headerColor[theme],
+                "color": textColor[theme],
+            },
+            children=[
+                html.Img(
+                    src=app.get_asset_url("logo-mini.png"),
+                    className="logo three columns",
+                ),
+                html.H6("Dash System Monitor",
+                        className="title six columns"),
+                html.Div(
+                    className="three columns",
+                    style={"float": "left"},
+                    children=[
+                        daq.ToggleSwitch(
+                            id="toggleTheme",
+                            label=["Light", "Dark"],
+                            style={
+                                "margin": "auto",
+                                "width": "65%",
+                                "color": textColor[theme],
+                            },
+                            value=False,
+                            size=35,
+                        )
+                    ],
+                ),
+            ],
+        ),
+        html.P(id="summary", className="mono retro line-1",
+               children=getEverything()),
+        html.Div(
+            id="bodySection",
+            children=bodyLayoutGen(),
+            className="flex-display",
+            style={"backgroundColor": backgroundColor[theme], "padding": "2%"},
+        ),
+    ],
 )
 
-@app.callback(Output('cpuTempLiveLed', 'value'),
-              [Input('intervalCounter', 'n_intervals')])
-def updateCpuTempLiveLed(n):
+# ======= Dark/light themes callbacks =======
+
+
+@app.callback(
+    [Output("bodySection", "style"),
+     Output("bodySection", "children")],
+    [Input("toggleTheme", "value")],
+    [State("bodySection", "style")],
+)
+def page_style(value, style_dict):
+    """update the theme of the app"""
+    global theme
+    if value:
+        theme = "dark"
+    else:
+        theme = "light"
+
+    style_dict["color"] = textColor[theme]
+    style_dict["backgroundColor"] = backgroundColor[theme]
+    return style_dict, bodyLayoutGen()
+
+
+@app.callback(
+    Output("header", "style"),
+    [Input("toggleTheme", "value")],
+    [State("header", "style")],
+)
+def header_style(value, style_dict):
+    """update the theme of header"""
+    global theme
+    if value:
+        theme = "dark"
+    else:
+        theme = "light"
+
+    style_dict["color"] = textColor[theme]
+    style_dict["backgroundColor"] = headerColor[theme]
+    return style_dict
+
+
+@app.callback([Output('gpuTempLiveLed', 'value'),
+               Output('cpuTempLiveLed', 'value')],
+              [Input('tempInterval', 'n_intervals')],
+              [State('tempStopButton', 'on')])
+def getTempData(n, stop):
+    if not stop:
+        return 0, 0
+
     cpuTemp = getCpuTemp()
-    return cpuTemp
+    tempDict["cpu"].append(cpuTemp)
 
-@app.callback(Output('gpuTempLiveLed', 'value'),
-              [Input('intervalCounter', 'n_intervals')])
-def updateGpuTempLiveLed(n):
     gpuTemp = getGpuTemp()
-    return gpuTemp
+    tempDict["gpu"].append(gpuTemp)
 
-# Multiple components can update everytime interval gets fired.
-@app.callback(Output('tempLiveLed', 'value'),
-              [Input('intervalCounter', 'n_intervals')],
-              [State('cpuTempLiveLed', 'value'),
-               State('gpuTempLiveLed', 'value')])
-def updateTempLiveGraph(n, cpuTemp, gpuTemp):
-    data = {
-        'cpuTemp': [],
-        'gpuTemp': [],
-        'time': []
+    time = datetime.datetime.now()
+    tempDict["time"].append(time)
+
+    return gpuTemp, cpuTemp
+
+
+@app.callback(Output('tempDataResetButton', 'n_clicks_timestamp'),
+              [Input('tempDataResetButton', 'n_clicks')])
+def resetTempData(n_clicks):
+    if n_clicks == 0 or n_clicks is None:
+        PreventUpdate()
+
+    global tempDict
+    tempDict = {"cpu": [], "gpu": [], 'time': []}
+    return 0
+
+
+@app.callback(Output('tempLiveGraph', 'figure'),
+              [Input('tempInterval', 'n_intervals')])
+def updateTempLiveGraph(n):
+    data = [
+        {
+            'x': tempDict['time'],
+            'y': tempDict['cpu'],
+            'name': 'Cpu temp',
+            'mode': 'lines',
+            'type': 'scatter'
+        },
+        {
+            'x': tempDict['time'],
+            'y': tempDict['gpu'],
+            'name': 'Gpu Temp',
+            'mode': 'lines',
+            'type': 'scatter'
+        }
+    ]
+    figure = {
+        "data": data,
+        "layout": dict(
+            paper_bgcolor=bckLightColor[theme],
+            plot_bgcolor=bckLightColor[theme],
+            automargin=True,
+            font=dict(color=textColor[theme], size=12),
+            xaxis={
+                "color": lightColor[theme],
+                "gridcolor": lightColor[theme],
+            },
+            yaxis={
+                "color": lightColor[theme],
+                "gridcolor": lightColor[theme],
+            },
+        ),
     }
 
-    # Collect some data
-    for i in range(180):
-        time = datetime.datetime.now() - datetime.timedelta(seconds=i*20)
-        cpuTemp = getCpuTemp()
-        gpuTemp = getGpuTemp()
-        data['cpuTemp'].append(cpuTemp)
-        data['gpuTemp'].append(gpuTemp)
-        data['time'].append(time)
+    return figure
 
-    # Create the graph with subplots
-    fig = plotly.tools.make_subplots(rows=2, cols=1, vertical_spacing=0.2)
-    fig['layout']['margin'] = {
-        'l': 30, 'r': 10, 'b': 30, 't': 10
+
+@app.callback([Output('memUsageLiveGauge', 'value'),
+               Output('cpuUsageLiveGauge', 'value')],
+              [Input('usageInterval', 'n_intervals')],
+              [State('usageStopButton', 'on')])
+def getUsageData(n, stop):
+    if not stop:
+        return 0, 0
+
+    cpuUsage = getCpuUsage()
+    usageDict["cpu"].append(cpuUsage)
+
+    memUsage = getMemUsage()
+    usageDict["mem"].append(memUsage)
+
+    time = datetime.datetime.now()
+    usageDict["time"].append(time)
+
+    return memUsage, cpuUsage
+
+
+@app.callback(Output('usageDataResetButton', 'n_clicks_timestamp'),
+              [Input('usageDataResetButton', 'n_clicks')])
+def resetUsageData(n_clicks):
+    if n_clicks == 0 or n_clicks is None:
+        PreventUpdate()
+
+    global usageDict
+    usageDict = {"cpu": [], "mem": [], 'time': []}
+    return 0
+
+
+@app.callback(Output('usageLiveGraph', 'figure'),
+              [Input('usageInterval', 'n_intervals')])
+def updateUsageLiveGraph(n):
+    data = [
+        {
+            'x': usageDict['time'],
+            'y': usageDict['cpu'],
+            'name': 'Cpu usage',
+            'mode': 'lines',
+            'type': 'scatter'
+        },
+        {
+            'x': usageDict['time'],
+            'y': usageDict['mem'],
+            'name': 'Mem Usage',
+            'mode': 'lines',
+            'type': 'scatter'
+        }
+    ]
+    figure = {
+        "data": data,
+        "layout": dict(
+            paper_bgcolor=bckLightColor[theme],
+            plot_bgcolor=bckLightColor[theme],
+            automargin=True,
+            font=dict(color=textColor[theme], size=12),
+            xaxis={
+                "color": lightColor[theme],
+                "gridcolor": lightColor[theme],
+            },
+            yaxis={
+                "color": lightColor[theme],
+                "gridcolor": lightColor[theme],
+            },
+        ),
     }
-    fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
 
-    fig.append_trace({
-        'x': data['time'],
-        'y': data['cpuTemp'],
-        'name': 'Cpu temp',
-        'mode': 'lines+markers',
-        'type': 'scatter'
-    }, 1, 1)
-    fig.append_trace({
-        'x': data['time'],
-        'y': data['gpuTemp'],
-        'text': data['time'],
-        'name': 'Gpu Temp',
-        'mode': 'lines+markers',
-        'type': 'scatter'
-    }, 2, 1)
-
-    return fig
+    return figure
 
 
 if __name__ == '__main__':
--- getOsInfo.py
+++ getOsInfo.py
@@ -1,7 +1,88 @@
-import random
+import psutil
+import os
+from requests import get
+
+
+def getEverything():
+    """Find and return everything
+
+    Returns:
+        string -- Everything
+    """
+    return os.popen('inxi -F -c 0').read()
+
+
+def getExternalIP():
+    """Find and return External Ip Address
+
+    Returns:
+        string -- External IP Address
+    """
+    return get('https://api.ipify.org').text
+
+
+def getOSVersion():
+    """Find and return OS Version
+
+    Returns:
+        string -- OS Version
+    """
+    return os.uname().version
+
+
+def getHostName():
+    """Find and return Host Name
+
+    Returns:
+        string -- Host Name
+    """
+    return os.uname().nodename
+
+
+def getOS():
+    """Find and return OS
+
+    Returns:
+        string -- OS Name
+    """
+    return os.uname().sysname
+
+
+def getCpuUsage():
+    """Find and return CPU Usage
+
+    Returns:
+        int -- Cpu Usage
+    """
+    cpuUsage = psutil.cpu_percent()
+    return int(cpuUsage)
+
+
+def getMemUsage():
+    """Find and return Mem Usage
+
+    Returns:
+        int -- Mem Usage
+    """
+    memUsage = psutil.virtual_memory().percent
+    return int(memUsage)
+
 
 def getCpuTemp():
-    return random.randint(80, 90)
+    """Find and return CPU Temp
+
+    Returns:
+        int -- Cpu Temp
+    """
+    cpuTemp = psutil.sensors_temperatures()["coretemp"][0].current
+    return int(cpuTemp)
+
 
 def getGpuTemp():
-    return random.randint(35, 45)
+    """Find and return GPU Temp
+
+    Returns:
+        int -- Gpu Temp
+    """
+    gpuTemp = psutil.sensors_temperatures()["coretemp"][2].current
+    return int(gpuTemp)
